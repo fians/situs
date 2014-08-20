17d54075eaa8ed652365adc0d62639d6ba67c7d2
Alfiana E. Sibuea <alfian.sibuea@gmail.com>
2014-08-20 19:29:37 +0700

    Add code syntax highlighting using highlight.js

--- src/handlebars.js
+++ src/handlebars.js
@@ -4,6 +4,7 @@
  */
 
 var handlebars  = require('handlebars');
+var hljs        = require('highlight.js');
 
 /**
  * Escape string
@@ -14,6 +15,22 @@ handlebars.registerHelper('escape', function(options) {
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;")
         .replace(/"/g, "&quot;");
+        
+});
+
+/**
+ * Highlight code using highlight.js
+ */
+handlebars.registerHelper('code', function(options) {
+
+    if (options.hash.hasOwnProperty('lang')) {
+        var lang = options.hash.lang;
+        string = '<pre><code class="'+lang+'">' + hljs.highlight(lang, options.fn(this)).value;
+    } else {
+        string = '<pre><code>' + hljs.highlightAuto(options.fn(this)).value;
+    }
+
+    return string += '</code></pre>';
 });
 
 module.exports = handlebars;
